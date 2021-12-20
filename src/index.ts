ec573a461b586dd09b84e4b2fdc2c2113b457788
Renan Ariceto <renan.ariceto@zenvia.com>
2021-12-20 10:29:54 -0300

    add cors option

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,23 @@ async function start() {
         const app = express();
 
         app.use(express.json())
-        app.use(cors())
-        app.use(indexRoutes);
+
+        const options: cors.CorsOptions = {
+            allowedHeaders: [
+                'Origin',
+                'X-Requested-With',
+                'Content-Type',
+                'Accept',
+                'X-Access-Token',
+            ],
+            credentials: true,
+            methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+            origin: '*',
+            preflightContinue: false,
+        };
+          
+        app.use(cors(options))
+        app.use(indexRoutes)
 
         app.listen(process.env.PORT || 3000, () => {
             console.log(`App running on port ${process.env.PORT}`)
